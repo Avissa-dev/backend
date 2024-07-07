adc8ae70ac5dc05dd5bd2d647c38027316523ead
DieR-G <00043920@uca.edu.sv>
2024-07-07 00:00:30 -0600

    fix points order

--- sql/direct_transfers_geojson.sql
+++ sql/direct_transfers_geojson.sql
@@ -8,8 +8,11 @@ SELECT
 	json_build_object(
 		'type', 'Feature',
         'geometry', json_build_object(
-			'type', 'LineString',
-			'coordinates', st_asgeojson(st_linesubstring(f.geom, st_linelocatepoint(f.geom, st_geometryfromtext('POINT(' || start_point || ')', 4326)), st_linelocatepoint(f.geom, st_geometryfromtext('POINT(' || end_point || ')', 4326))))::json->'coordinates'
+			'type', 'MultiLineString',
+			'coordinates', st_asgeojson(st_linesubstring(f.geom,
+				least(st_linelocatepoint(f.geom, st_geometryfromtext('POINT(' || start_point || ')', 4326)), st_linelocatepoint(f.geom, st_geometryfromtext('POINT(' || end_point || ')', 4326))),
+				greatest(st_linelocatepoint(f.geom, st_geometryfromtext('POINT(' || start_point || ')', 4326)), st_linelocatepoint(f.geom, st_geometryfromtext('POINT(' || end_point || ')', 4326)))
+			))::json->'coordinates'
 		),
 		'properties', json_build_object(
 			'bus_number', 1,
@@ -20,7 +23,10 @@ SELECT
 	)
 FROM get_filtered_direct_routes(start_point, end_point) f
 ORDER BY (
-	st_length(st_linesubstring(f.geom, st_linelocatepoint(f.geom, st_geometryfromtext('POINT(' || start_point || ')', 4326)), st_linelocatepoint(f.geom, st_geometryfromtext('POINT(' || end_point || ')', 4326))))
+	st_length(st_linesubstring(f.geom,
+				least(st_linelocatepoint(f.geom, st_geometryfromtext('POINT(' || start_point || ')', 4326)), st_linelocatepoint(f.geom, st_geometryfromtext('POINT(' || end_point || ')', 4326))),
+				greatest(st_linelocatepoint(f.geom, st_geometryfromtext('POINT(' || start_point || ')', 4326)), st_linelocatepoint(f.geom, st_geometryfromtext('POINT(' || end_point || ')', 4326)))
+			))
 )
 limit 15;
 END; 
